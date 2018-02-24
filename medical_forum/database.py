a93429a3ed68aa91e0ccfbb519049c24a3bb8c33
Yazan Barhoush <ybarhoush@gmail.com>
2018-02-24 08:36:13 -0500

    class Engine ... create_users_table, create_patients_profile_table

--- medical_forum/database.py
+++ medical_forum/database.py
@@ -1,10 +1,241 @@
 '''
-Created on 24.02.2018
+Created on 13.02.2013
 
 Modified on 25.02.2018
 
 Provides the database API to access the forum persistent data.
 
+@author: ivan
+@author: mika
 @author: yazan
 '''
 
+from datetime import datetime
+import time, sqlite3, re, os
+#Default paths for .db and .sql files to create and populate the database.
+DEFAULT_DB_PATH = 'db/forum.db'
+DEFAULT_SCHEMA = "db/forum_schema_dump.sql"
+DEFAULT_DATA_DUMP = "db/forum_data_dump.sql"
+
+# Copied Class from Exercise 1
+# Unless it is stated in the comment before the method, the method is copied
+class Engine(object):
+    '''
+    Abstraction of the database.
+
+    It includes tools to create, configure,
+    populate and connect to the sqlite file. You can access the Connection
+    instance, and hence, to the database interface itself using the method
+    :py:meth:`connection`.
+
+    :Example:
+
+    >>> engine = Engine()
+    >>> con = engine.connect()
+
+    :param db_path: The path of the database file (always with respect to the
+        calling script. If not specified, the Engine will use the file located
+        at *db/forum.db*
+
+    '''
+    def __init__(self, db_path=None):
+        '''
+        '''
+
+        super(Engine, self).__init__()
+        if db_path is not None:
+            self.db_path = db_path
+        else:
+            self.db_path = DEFAULT_DB_PATH
+
+    def connect(self):
+        '''
+        Creates a connection to the database.
+
+        :return: A Connection instance
+        :rtype: Connection
+
+        '''
+        return Connection(self.db_path)
+
+    def remove_database(self):
+        '''
+        Removes the database file from the filesystem.
+
+        '''
+        if os.path.exists(self.db_path):
+            #THIS REMOVES THE DATABASE STRUCTURE
+            os.remove(self.db_path)
+
+    def clear(self):
+        '''
+        Purge the database removing all records from the tables. However,
+        it keeps the database schema (meaning the table structure)
+
+        '''
+        keys_on = 'PRAGMA foreign_keys = ON'
+        #THIS KEEPS THE SCHEMA AND REMOVE VALUES
+        con = sqlite3.connect(self.db_path)
+        #Activate foreing keys support
+        cur = con.cursor()
+        cur.execute(keys_on)
+        with con:
+            cur = con.cursor()
+            cur.execute("DELETE FROM messages")
+            cur.execute("DELETE FROM users")
+            #NOTE since we have ON DELETE CASCADE BOTH IN users_profile AND
+            #friends, WE DO NOT HAVE TO WORRY TO CLEAR THOSE TABLES.
+
+    #METHODS TO CREATE AND POPULATE A DATABASE USING DIFFERENT SCRIPTS
+    def create_tables(self, schema=None):
+        '''
+        Create programmatically the tables from a schema file.
+
+        :param schema: path to the .sql schema file. If this parmeter is
+            None, then *db/forum_schema_dump.sql* is utilized.
+
+        '''
+        con = sqlite3.connect(self.db_path)
+        if schema is None:
+            schema = DEFAULT_SCHEMA
+        try:
+            with open(schema, encoding="utf-8") as f:
+                sql = f.read()
+                cur = con.cursor()
+                cur.executescript(sql)
+        finally:
+            con.close()
+
+    def populate_tables(self, dump=None):
+        '''
+        Populate programmatically the tables from a dump file.
+
+        :param dump:  path to the .sql dump file. If this parmeter is
+            None, then *db/forum_data_dump.sql* is utilized.
+
+        '''
+        keys_on = 'PRAGMA foreign_keys = ON'
+        con = sqlite3.connect(self.db_path)
+        #Activate foreing keys support
+        cur = con.cursor()
+        cur.execute(keys_on)
+        #Populate database from dump
+        if dump is None:
+            dump = DEFAULT_DATA_DUMP
+        try:
+            with open (dump, encoding="utf-8") as f:
+                sql = f.read()
+                cur = con.cursor()
+                cur.executescript(sql)
+        finally:
+            con.close()
+
+    #METHODS TO CREATE THE TABLES PROGRAMMATICALLY WITHOUT USING SQL SCRIPT
+    ### Modified from create_messages_table ###
+    def create_messages_table(self):
+        '''
+        Create the table ``messages`` programmatically, without using .sql file.
+
+        Print an error message in the console if it could not be created.
+
+        :return: ``True`` if the table was successfully created or ``False``
+            otherwise.
+
+        '''
+        keys_on = 'PRAGMA foreign_keys = ON'
+        stmnt = 'CREATE TABLE messages(message_id INTEGER PRIMARY KEY, \
+        user_id INTEGER, \
+        username TEXT, \
+        reply_to INTEGER, \
+        title TEXT, \
+        body TEXT, \
+        views INTEGER, \
+        created_time INTEGER, \
+        FOREIGN KEY(reply_to) \
+        REFERENCES messages(message_id) ON DELETE CASCADE \
+        FOREIGN KEY(user_id,username) \
+        REFERENCES users(user_id, username) ON DELETE SET NULL)'
+        con = sqlite3.connect(self.db_path)
+        with con:
+            #Get the cursor object.
+            #It allows to execute SQL code and traverse the result set
+            cur = con.cursor()
+            try:
+                cur.execute(keys_on)
+                #execute the statement
+                cur.execute(stmnt)
+            except sqlite3.Error as excp:
+                print("Error %s:" % excp.args[0])
+                return False
+        return True
+    ### Modified from create_users_table
+    def create_users_table(self):
+        '''
+        Create the table ``users`` programmatically, without using .sql file.
+
+        Print an error message in the console if it could not be created.
+
+        :return: ``True`` if the table was successfully created or ``False``
+            otherwise.
+
+        '''
+        keys_on = 'PRAGMA foreign_keys = ON'
+        stmnt = 'CREATE TABLE users(user_id INTEGER PRIMARY KEY,\
+        user_type TEXT, \
+        username TEXT UNIQUE, \
+        registration_date INTEGER,\
+        last_log INTEGER, \
+        msg_count INTEGER,\
+        UNIQUE(user_id, username))'
+        #Connects to the database. Gets a connection object
+        con = sqlite3.connect(self.db_path)
+        with con:
+            #Get the cursor object.
+            #It allows to execute SQL code and traverse the result set
+            cur = con.cursor()
+            try:
+                cur.execute(keys_on)
+                #execute the statement
+                cur.execute(stmnt)
+            except sqlite3.Error as excp:
+                print("Error %s:" % excp.args[0])
+                return False
+        return True
+    ### Modified from create_patients_profile_table
+    def create_patients_profile_table(self):
+        '''
+        Create the table ``patients_profile`` programmatically, without using
+        .sql file.
+
+        Print an error message in the console if it could not be created.
+
+        :return: ``True`` if the table was successfully created or ``False``
+            otherwise.
+
+        '''
+        keys_on = 'PRAGMA foreign_keys = ON'
+
+        stmnt = 'CREATE TABLE users_profile(user_id INTEGER PRIMARY KEY, \
+                    firstname TEXT, lastname TEXT, \
+                    email TEXT, website TEXT, \
+                    picture TEXT, mobile TEXT, \
+                    skype TEXT, birthday TEXT, \
+                    residence TEXT, gender TEXT, \
+                    signature TEXT, avatar TEXT, \
+                    FOREIGN KEY(user_id) REFERENCES users(user_id) \
+                    ON DELETE CASCADE)'
+        #Connects to the database. Gets a connection object
+        #Connects to the database. Gets a connection object
+        con = sqlite3.connect(self.db_path)
+        with con:
+            #Get the cursor object.
+            #It allows to execute SQL code and traverse the result set
+            cur = con.cursor()
+            try:
+                cur.execute(keys_on)
+                #execute the statement
+                cur.execute(stmnt)
+            except sqlite3.Error as excp:
+                print("Error %s:" % excp.args[0])
+                return False
+        return True
