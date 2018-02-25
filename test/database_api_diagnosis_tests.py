1ab4e92e0252aa236bcc9f5b489c743af9666fff
Issam <assam.bj@gmail.com>
2018-02-25 22:13:21 +0200

    all diagnosis unittests

--- test/database_api_diagnosis_tests.py
+++ test/database_api_diagnosis_tests.py
@@ -11,6 +11,31 @@ import unittest
 # import helper different methods
 from .utils import *
 
+# declare constants
+DIAGNOSIS_1_ID = 'diagnosis-1'
+DIAGNOSIS_1 = {'user_id': 1,
+               'message_id': 'msg-7',
+               'disease': 'blood',
+               'diagnosis_description': 'Anne bought new car. Tony bought new car. Anne has free time. '
+                                        'John has free time. Tony bought new car. '}
+DIAGNOSIS_2_ID = 'diagnosis-12'
+DIAGNOSIS_2 = {'user_id': 9,
+               'message_id': 'msg-3',
+               'disease': 'eyes',
+               'diagnosis_description': 'Tony has free time. Anne is shopping. Tony bought new car. '
+                                        'John bought new car. John bought new car. '}
+
+DOCTOR_ID = 4
+NOT_DOCTOR_ID = 1
+NEW_DIAGNOSIS = {'user_id': DOCTOR_ID,
+                 'message_id': 'msg-3',
+                 'disease': 'flu',
+                 'diagnosis_description': 'You need to stay and calm and not panic.'}
+
+BAD_DIAGNOSIS_ID = '100'
+NON_EXIST_DIAGNOSIS_ID = 'diagnosis-100'
+
+
 class DatabaseDiagnosisTestCase(unittest.TestCase):
     """
     Test cases for different database diagnosis table methods
@@ -48,81 +73,69 @@ class DatabaseDiagnosisTestCase(unittest.TestCase):
         self.connection.close()
         ENGINE.clear()
 
-    def test_users_table_populated(self):
+    def test_diagnosis_table_populated(self):
         """
-        Check that the users table has been populated with default data successfully.
+        Check that the diagnosis table has been populated with default data successfully.
          """
-        print('(' + self.test_users_table_populated.__name__ + ')', self.test_users_table_populated.__doc__)
-        test_table_populated(self, USERS_TABLE, INITIAL_USERS_COUNT)
+        print('(' + self.test_diagnosis_table_populated.__name__ + ')', self.test_diagnosis_table_populated.__doc__)
+        test_table_populated(self, DIAGNOSIS_TABLE, INITIAL_DIAGNOSIS_COUNT)
 
-    def test_users_profile_table_populated(self):
+    def test_create_diagnosis_object(self):
         """
-        Check that the users_profile table has been populated with default data successfully.
-         """
-        print('(' + self.test_users_profile_table_populated.__name__ + ')',
-              self.test_users_profile_table_populated.__doc__)
-        test_table_populated(self, USERS_PROFILE_TABLE, INITIAL_USERS_PROFILE_COUNT)
-
-    def test_create_user_object(self):
+        Check that the method create_diagnosis_object works return proper values for diagnosis id 1
         """
-        Check that the method create_user_object works return proper values.
-        """
-        print('(' + self.test_create_user_object.__name__+')', self.test_create_user_object.__doc__)
+        print('(' + self.test_create_diagnosis_object.__name__+')', self.test_create_diagnosis_object.__doc__)
         # Query to get users and users_profile for the patient
-        query = 'SELECT users.*, users_profile.* FROM users, users_profile \
-                 WHERE users.user_id = users_profile.user_id'
+        query = 'SELECT * FROM diagnosis WHERE diagnosis_id = 1'
         # assert if result doesn't contain patient
-        self.assertDictContainsSubset(self.connection._create_user_object(execute_query(self, query, 'one')), PATIENT)
+        self.assertDictContainsSubset(self.connection._create_diagnosis_object(execute_query(self, query, 'one')),
+                                      DIAGNOSIS_1)
 
-    def test_create_user_list_object(self):
+    def test_get_diagnosis(self):
         """
-        Check that the method create_user_object works return proper values.
+        Test get_diagnosis with diagnosis_id
         """
-        print('(' + self.test_create_user_object.__name__+')', self.test_create_user_object.__doc__)
-        # Query to get users and users_profile for the patient
-        query = 'SELECT users.*, users_profile.* FROM users, users_profile \
-                 WHERE users.user_id = users_profile.user_id'
-        # assert if result doesn't contain patient
-        self.assertDictContainsSubset(self.connection._create_user_object(execute_query(self, query, 'one')), PATIENT)
-
-    def test_get_user(self):
-        """
-        Test get_user for patient with username PoorGuy and doctor with username Clarissa
-        """
-        print('(' + self.test_get_user.__name__+')', self.test_get_user.__doc__)
+        print('(' + self.test_get_diagnosis.__name__+')', self.test_get_diagnosis.__doc__)
         # test for patient
-        self.assertDictContainsSubset(self.connection.get_user(PATIENT_USERNAME), PATIENT)
-        # test for doctor
-        self.assertDictContainsSubset(self.connection.get_user(DOCTOR_USERNAME), DOCTOR)
+        self.assertDictContainsSubset(self.connection.get_diagnosis(DIAGNOSIS_1_ID), DIAGNOSIS_1)
 
-    def test_get_user_non_exist_id(self):
+    def test_get_bad_diagnosis(self):
         """
-        Test get_user with  msg-200 (no-existing)
+        Test get_diagnosis with  diagnosis-100 (no-existing)
         """
-        print('(' + self.test_get_user_non_exist_id.__name__+')', self.test_get_user_non_exist_id.__doc__)
-        self.assertIsNone(self.connection.get_user(NON_EXIST_PATIENT_USERNAME))
+        print('(' + self.test_get_bad_diagnosis.__name__+')', self.test_get_bad_diagnosis.__doc__)
+        with self.assertRaises(ValueError):
+            self.connection.get_diagnosis(BAD_DIAGNOSIS_ID)
 
-    def test_append_user(self):
+    def test_get_non_exist_diagnosis(self):
         """
-        Test that a new patient/doctor can be added and check its data after adding
+        Test get_diagnosis with  diagnosis-100 (no-existing)
         """
-        print('(' + self.test_append_user.__name__+')', self.test_append_user.__doc__)
-        new_username = self.connection.append_user(NEW_PATIENT_USERNAME, NEW_PATIENT)
+        print('(' + self.test_get_non_exist_diagnosis.__name__+')', self.test_get_non_exist_diagnosis.__doc__)
+        self.assertIsNone(self.connection.get_diagnosis(NON_EXIST_DIAGNOSIS_ID))
+
+    def test_append_diagnosis(self):
+        """
+        Test that a new diagnosis can be added and check its data after adding
+        """
+        print('(' + self.test_append_diagnosis.__name__+')', self.test_append_diagnosis.__doc__)
+        new_diagnosis_id = self.connection.create_diagnosis(NEW_DIAGNOSIS)
         # test appended ok
-        self.assertIsNotNone(new_username)
-        # check appended the same user data
-        self.assertEqual(new_username, NEW_PATIENT_USERNAME)
+        self.assertIsNotNone(new_diagnosis_id)
         # check the added user in db has the same data
-        get_new_patient = self.connection.get_user(new_username)
-        self.assertDictContainsSubset(NEW_PATIENT['restricted_profile'], get_new_patient['restricted_profile'])
-        self.assertDictContainsSubset(NEW_PATIENT['public_profile'], get_new_patient['public_profile'])
+        get_new_diagnosis = self.connection.get_diagnosis(new_diagnosis_id)
+        self.assertDictContainsSubset(NEW_DIAGNOSIS, get_new_diagnosis)
 
-    def test_append_existing_user(self):
+    def test_append_diagnosis_by_non_doctor(self):
         """
-        Test that I cannot add two users with the same name
+        Test that a new diagnosis cannot be added by a non-doctor user
         """
-        print('(' + self.test_append_existing_user.__name__+')', self.test_append_existing_user.__doc__)
-        self.assertIsNone(self.connection.append_user(PATIENT_USERNAME, NEW_PATIENT))
+        print('(' + self.test_append_diagnosis_by_non_doctor.__name__+')',
+              self.test_append_diagnosis_by_non_doctor.__doc__)
+        no_doctor_diagnosis = NEW_DIAGNOSIS
+        no_doctor_diagnosis['user_id'] = NOT_DOCTOR_ID
+        with self.assertRaises(ValueError):
+            self.connection.create_diagnosis(no_doctor_diagnosis)
 
 
 if __name__ == '__main__':
