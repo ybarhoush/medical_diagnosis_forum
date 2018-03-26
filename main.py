0308c1f99b76b98348a2909ce9528acda80b8784
Yazan Barhoush <ybarhoush@gmail.com>
2018-03-26 15:23:26 +0300

    added main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# from medical_forum.resources import app
+
+#Start the application
+#DATABASE SHOULD HAVE BEEN POPULATED PREVIOUSLY
+#if __name__ == '__main__':
+    #Debug true activates automatic code reloading and improved error messages
+    #app.run(debug=True)
